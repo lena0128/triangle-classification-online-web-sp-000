ddbf4328aa5adf88ff879f07e0d6be90d4f455c4
Lena Hou <lenahou2013@gmail.com>
2021-02-22 19:26:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,15 +9,25 @@ class Triangle
   end
 
 def kind
+  :equilateral
+  :isosceles
+  :scalene
   if side_1 + side_2 < side_3 || side_2 + side_3 < side_1 || side_1 + side_3 < side_2
     begin
     raise TriangleError
   rescue TriangleError => error
     puts error.message
 else
-  :equilateral || :isosceles || :scalene
+  :equilateral
+  :isosceles
+  :scalene
 end
 end
 
+class TriangleError < StandardError
+  def message
+    puts "You are wrong!"
+  end
+end
 
 end
