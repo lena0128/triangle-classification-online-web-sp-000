9bc2b40ea5912dc61f4997b42f23969949d31d88
Lena Hou <lenahou2013@gmail.com>
2021-02-22 19:39:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,16 +6,18 @@ class Triangle
     @side_1 = side_1
     @side_2 = side_2
     @side_3 = side_3
+
+    if side_1 + side_2 =< side_3 || side_2 + side_3 =< side_1 || side_1 + side_3 =< side_2
+      raise TriangleError
+    end
+
+    if side_1 <= 0 || side_2 <= 0 || side_3 <= 0
+      raise TriangleError
+    end
   end
 
 def kind
-  if side_1 + side_2 =< side_3 || side_2 + side_3 =< side_1 || side_1 + side_3 =< side_2
-    begin
-      raise TriangleError
-    rescue TriangleError => error
-      puts error.message
-    end
-  elsif side_1 == side_2 && side_2 == side_3 && side_1 == side_3
+  if side_1 == side_2 && side_2 == side_3 && side_1 == side_3
     :equilateral
   elsif side_1 == side_2 || side_2 == side_3 || side_1 == side_3
     :isosceles
@@ -28,6 +30,5 @@ class TriangleError < StandardError
   def message
     puts "You are wrong!"
   end
-end
 
 end
