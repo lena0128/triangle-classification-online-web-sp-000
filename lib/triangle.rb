51b0ac9a1b181703df51c22c021b6995249201ac
Lena Hou <lenahou2013@gmail.com>
2021-02-22 19:21:54 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,10 @@ class Triangle
 def kind
 
   if side_1 + side_2 < side_3 || side_2 + side_3 < side_1 || side_1 + side_3 < side_2
-  raise TriangleError
+    begin
+    raise TriangleError
+  rescue TriangleError => error
+    puts error.message
 else
   :equilateral
   :isosceles
