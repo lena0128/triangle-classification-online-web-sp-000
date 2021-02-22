40df62c2d9e75936049af3fecf7d0f1c47d38177
Lena Hou <lenahou2013@gmail.com>
2021-02-22 19:33:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,13 +9,13 @@ class Triangle
   end
 
 def kind
-
   if side_1 + side_2 < side_3 || side_2 + side_3 < side_1 || side_1 + side_3 < side_2
     begin
       raise TriangleError
     rescue TriangleError => error
       puts error.message
     end
+  end
   elsif side_1 == side_2 && side_2 == side_3 && side_1 == side_3
     :equilateral
   elsif side_1 == side_2 || side_ 2 == side_ 3 || side_1 == side_3
