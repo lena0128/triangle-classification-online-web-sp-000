9e1643166e7a72b06f467660772ae31b349a589d
Lena Hou <lenahou2013@gmail.com>
2021-02-22 19:33:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,6 @@ def kind
     rescue TriangleError => error
       puts error.message
     end
-  end
   elsif side_1 == side_2 && side_2 == side_3 && side_1 == side_3
     :equilateral
   elsif side_1 == side_2 || side_ 2 == side_ 3 || side_1 == side_3
