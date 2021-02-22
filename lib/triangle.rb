5d5d3c28bca0933a4043b273ad98b016b4ce072f
Lena Hou <lenahou2013@gmail.com>
2021-02-22 19:37:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   end
 
 def kind
-  if side_1 + side_2 < side_3 || side_2 + side_3 < side_1 || side_1 + side_3 < side_2
+  if side_1 + side_2 =< side_3 || side_2 + side_3 =< side_1 || side_1 + side_3 =< side_2
     begin
       raise TriangleError
     rescue TriangleError => error
